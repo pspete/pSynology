1ec9418c97885f5857b9177941cda9b65ab81e62
Pete Maan <pete.maan+github@gmail.com>
2019-05-13 22:20:20 +0100

    :tada: Initial commit.
    Initial module structure.

---
+++ pSynology/pSynology.psm1
@@ -0,0 +1,50 @@
+﻿<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.EXAMPLE
+
+.INPUTS
+
+.OUTPUTS
+
+.NOTES
+
+.LINK
+
+#>
+[CmdletBinding()]
+param(
+
+	[bool]$DotSourceModule = $false
+
+)
+
+#Get function files
+Write-Verbose $PSScriptRoot
+
+Get-ChildItem $PSScriptRoot\ -Recurse -Filter "*.ps1" -Exclude "*.ps1xml" |
+
+ForEach-Object {
+
+	if ($DotSourceModule) {
+		. $_.FullName
+	} else {
+		$ExecutionContext.InvokeCommand.InvokeScript(
+			$false,
+			(
+				[scriptblock]::Create(
+					[io.file]::ReadAllText(
+						$_.FullName,
+						[Text.Encoding]::UTF8
+					)
+				)
+			),
+			$null,
+			$null
+		)
+
+	}
+
+}
